cd26c071b4de7ac4438d81220240628e1c4467cf
Ryan <ryan.warnock@dpc.wa.gov.au>
2023-04-12 11:30:05 +0800

    Added code comments

--- BuildCollatedARMFile.py
+++ BuildCollatedARMFile.py
@@ -7,16 +7,24 @@ TEMPLATE_FILE = 'template.json'
 OUTPUT_FILE = 'CollatedDeployment.json'
 REPO = 'WASOCAutomationPlaybook/TaskAutomations' # Change to WAGOV [repo/folder] the rules are being deployed from
 
+# Load the template file
 template = []
 with open(TEMPLATE_FILE, 'r') as f:
     template = json.load(f)
 
+# Create a list of the automations that will be added to the template folder
+# by iterating through the `REPO` folder. This enforces a strict naming convention
+# where each folder name has an identical name to the file inside, with the file
+# having a `.json` extension. The script will fail if the naming convention isn't
+# followed
 rule_list = []
 for folder in os.listdir(REPO):
     # simple check to exclude files
     if not (folder.endswith('.json') or folder.endswith('.md') or folder.startswith('.')):
         rule_list.append(f'{folder}/{folder}.json')
 
+# Iterate over each rule discovered in the previous step and load the contents
+# into a `json` object.
 for rule in rule_list:
     json_location = f'{REPO}/{rule}'
     with open(json_location, 'r') as f:
